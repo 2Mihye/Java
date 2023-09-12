1afbf1b59ccf4fb58bbb353ce2ea567cff5c0563
Kaye <139112573+2Mihye@users.noreply.github.com>
2023-09-12 17:31:06 +0900

    2차원 배열

---
+++ chap21_2차원 배열/src/com/kh/arrayMain/ArrayMain.java
@@ -0,0 +1,13 @@
+package com.kh.arrayMain;
+
+import com.kh.arrayExam.ArrayExam;
+
+public class ArrayMain {
+	public static void main(String[] args) {
+		ArrayExam practice = new ArrayExam();
+		practice.practice1();
+		practice.practice2();
+		practice.practice3();
+		practice.practice4();
+	}
+}
