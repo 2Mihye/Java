9aea2f374ce8f397106b27480fb83b0dec176685
Kaye <139112573+2Mihye@users.noreply.github.com>
2023-09-13 12:50:03 +0900

    API

--- chap22_API/src/com/kh/api/exam1/CalendarMain.java
+++ chap22_API/src/com/kh/api/exam1/CalendarMain.java
@@ -1,9 +1,25 @@
 package com.kh.api.exam1;
 
+import java.util.Calendar;
+
 public class CalendarMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Calendar 클래스를 사용한 현재 날짜와 시간 가져오기
+		Calendar calendar = Calendar.getInstance(); // abstract이기 때문에 new를 쓰지 못함
+		int year = calendar.get(calendar.YEAR);
+		int month = calendar.get(calendar.MONTH);
+		int  day = calendar.get(calendar.DAY_OF_MONTH);
+		int hour = calendar.get(calendar.HOUR_OF_DAY);
+		int minute = calendar.get(calendar.MINUTE);
+		int second = calendar.get(calendar.SECOND);
+		System.out.println("calendar 클래스를 하용한 현재 날짜와 시간");
+		System.out.println(year + "년");
+		System.out.println(month + "월");
+		System.out.println(day + "일");
+		System.out.println(hour + "시");
+		System.out.println(minute + "분");
+		System.out.println(second + "초");
 
 	}
 
