e5371b194a0f5ba1f39fd036aacddbd536bc86e0
Kaye <139112573+2Mihye@users.noreply.github.com>
2023-09-14 12:49:59 +0900

    2023.09.14

--- chap24_입출력 (InputOutput)/src/com/kh/practice/PracticeMain.java
+++ chap24_입출력 (InputOutput)/src/com/kh/practice/PracticeMain.java
@@ -14,11 +14,13 @@ public class PracticeMain {
 	}
 	
 	public void practice1() { // 경로설정해서 파일 만들기
+		final String wpath = "C:\\Users\\user1\\Desktop\\FileName.txt"; // 파일 경로를 수정하지 못하게 적어둠
 		try {
 			FileWriter w = new FileWriter("C:\\Users\\user1\\Desktop\\FileName.txt");
 			w.write("이무진 목소리 좋다!");// 파일쓰기
 			w.close();// 파일 닫기
 			
+			// 절대 경로는 어디서 보던지 경로가 확실한 경로를 말함
 			FileReader reader = new FileReader ("C:\\Users\\user1\\Desktop\\FileName.txt");
 			BufferedReader bufferReader = new BufferedReader(reader);
 			String line;
