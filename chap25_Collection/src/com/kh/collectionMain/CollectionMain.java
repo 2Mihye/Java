ef04b9acecfbaa148a5f300248b7bc28a1b5f048
Kaye <139112573+2Mihye@users.noreply.github.com>
2023-09-15 12:30:20 +0900

    Map & HashMap

--- chap25_Collection/src/com/kh/collectionMain/CollectionMain.java
+++ chap25_Collection/src/com/kh/collectionMain/CollectionMain.java
@@ -1,18 +1,21 @@
 package com.kh.collectionMain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class CollectionMain {
 
 	public static void main(String[] args) {
-		// List
-		List<String> myList = new ArrayList<>();
+		// List 예제
+		List<String> myList = new ArrayList<>(); // List와 ArrayList import
 		myList.add("악뮤");
 		myList.add("아이유");
 		myList.add("백예린");
+		myList.add("아이유"); // 중복이지만 출력됨
 		
 		System.out.println("List : ");
 		// 향상된 for문 활용하여 출력
@@ -30,6 +33,23 @@ public class CollectionMain {
 		for(Integer number : mySet) {
 			System.out.println(number);
 		}
+		
+		// Map 예제
+		Map<String, Integer> myMap = new HashMap<>();
+		// 	  Map<String, Integer>
+		myMap.put("캐나다", 1);
+		myMap.put("한국",  82);
+		myMap.put("일본",  81);
+		myMap.put("미국",  1);
+		myMap.put("스페인", 34);
+		myMap.put("독일", 49);
+		myMap.put("한국",  3); // 중복출력이 아니라 가장 마지막에 들어간 요소로 출력이 된다.
+		
+		for(String country:myMap.keySet()) {
+			int code = myMap.get(country);
+			System.out.println(country + " : " + code);
+		}
+		
 	}
 
 }
--- chap25_Collection/src/com/kh/collectionPractice/PracticeCollection.java
+++ chap25_Collection/src/com/kh/collectionPractice/PracticeCollection.java
@@ -1,16 +1,19 @@
 package com.kh.collectionPractice;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class PracticeCollection {
 
 	public static void main(String[] args) {
 		PracticeCollection pc = new PracticeCollection();
-		pc.practiceList();
-		pc.practiceSet();
+		// pc.practiceList();
+		// pc.practiceSet();
+		pc.practiceMap();
 	}
 	
 	// String 중복된 값 포함해서 add 해보고 향상for문으로 출력
@@ -38,6 +41,22 @@ public class PracticeCollection {
 		System.out.println("Set : ");
 		for(String musician : mySet) {
 			System.out.println(musician);
+		}
+	}
+	
+	public void practiceMap() {
+		// 키와 값을 이용해서 map put 한다음 향상for문으로 출력해보기
+		Map<String, Integer> myMap = new HashMap<>();
+		myMap.put("짱구", 100);
+		myMap.put("철수", 85);
+		myMap.put("훈이", 90);
+		myMap.put("유리", 88);
+		myMap.put("맹구", 95);
+		myMap.put("철수", 99);
+		
+		for(String crayon : myMap.keySet()) {
+			int grade = myMap.get(crayon);
+			System.out.println(crayon + " " + grade + "점");
 		}
 	}
 
