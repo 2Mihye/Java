76bc548c59d9e762e4119b833b1f036fcb4e261d
Kaye <139112573+2Mihye@users.noreply.github.com>
2023-09-21 10:47:11 +0900

    JUnit

---
+++ chap35_JUnit/src/com/kh/jUnit/practice/ArrayPracticeTest.java
@@ -0,0 +1,31 @@
+package com.kh.jUnit.practice;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArrayPracticeTest {
+	private ArrayPractice arr;
+	
+	@Test
+	public void testArrayP1() {
+		int[] expected = {1,2,3,4,5,6,7,8,9,10};
+		// int[] actual = {1,2,3,4,5,6,7,8};
+		assertArrayEquals(expected, arr.getArray());
+	}
+	
+	@Test
+	public void testArrayP2() {
+		int[] expected = {1,2,3,5,6,7,8,9,10,11,12};
+		int[] actual = {0,1,2,3,4,5,6,7,8,9};
+		assertNotEquals(expected, actual);
+	}
+	
+	@Test
+	public void testArrayP3() {
+		int[] expected = null;
+		int[] actual = null;
+		assertEquals(expected, actual);
+		
+	}
+}
