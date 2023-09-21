e06f6909e33718d448edbaabe81f48df9c0a0849
Kaye <139112573+2Mihye@users.noreply.github.com>
2023-09-21 18:46:43 +0900

    Final Practice

--- chap36_Final_Practice/src/com/kh/finalSample/TcpClient.java
+++ chap36_Final_Practice/src/com/kh/finalSample/TcpClient.java
@@ -36,10 +36,6 @@ public class TcpClient { // 메서드를 하나 만들어줘야 함.
 
 
 
-
-
-
-
 /*
 import java.net.InetAddress;
 import java.net.Socket;
@@ -53,6 +49,42 @@ public class TcpClient { // 메서드를 하나 만들어줘야 함.
 		Socket socket = new Socket(serverIP, port);
 	} catch(Exception e){
 		e.printStackTrace();
+	}
+}
+*/
+
+
+/*>>>>>>>>>>>>>>>선생님 풀이<<<<<<<<<<<<<<<<<<<<<<<
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TcpClient { // 메서드를 하나 만들어줘야 함.
+	public static void main(String[] args) throws UnknownHostException {
+		int port = 3333;
+		String serverIP; // 내 아이피 주소
+		
+		serverIP = InetAddress.getLocalHost().getHostAddress(); // -> 우리가 사용하는 IP 주소를 자동으로 반환
+		System.out.println("serverIP");
+		try {
+			// serverIP = InetAddress.getLocalHost().getHostAddress();
+			// System.out.println("serverIP");
+			Socket socket = new Socket(serverIP, port);
+			System.out.println("server connect");
+			
+			// 클라이언트와 통신을 위한 입출력 스트림 설정
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			OutputStream out = socket.getOutputStream(); // import
+			
+			// 클라이언트에서 서버로 메세지 전송
+			String msg = "Hi, server";
+			out.write(msg.getBytes());
+			out.flush();
+			
+			socket.close();
+			 
+		} catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 }
 */
